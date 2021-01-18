8debf0489f6109b6a0c1cc2ddffe49581180b856
iagopedro <iagopedroltd19@gmail.com>
2021-01-18 16:40:19 -0300

    adjusting the landing page

--- src/pages/ArtistInfo/index.js
+++ src/pages/ArtistInfo/index.js
@@ -30,6 +30,10 @@ function ArtistInfo() {
   const [website, setWebsite] = useState('');
   const [facebook, setFacebook] = useState('');
 
+  function handleClick() {
+    window.location.href = facebook;
+  }
+
   useEffect(() => {
     async function loadInfo() {
       const response = await api.get('/search.php', {
@@ -75,10 +79,10 @@ function ArtistInfo() {
                   alt='Info' 
                   className='icons'
                 /> */}
-                <svg xmlns="http://www.w3.org/2000/svg" className='icons' width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
-                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                  <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" className='icons' width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                  </svg>
               </Button>
             </Col>
             <Col>
@@ -114,16 +118,16 @@ function ArtistInfo() {
           </Row>
           <Row>
             <Col>
-              <a href={facebook}>
+              {/* <a href={facebook}> */}
                 {/* <Image 
                   src={facebookIcon} 
                   alt='Facebook'
                   className='icons' 
                 /> */}
-                <svg xmlns="http://www.w3.org/2000/svg" className='icons' width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" className='icons' width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16" onClick={handleClick}>
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                 </svg>
-              </a>
+              {/* </a> */}
             </Col>
           </Row>
         </Container>
